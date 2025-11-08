2223360f0997cbfa33a5cbf5854fd24a78d5e730
Angel Manuel Ferrer Alvarez <angferalv@alum.us.es>
2025-11-08 19:29:11 +0100

    fix: :bug: test fixed

--- app/modules/dataset/tests/test_selenium.py
+++ app/modules/dataset/tests/test_selenium.py
@@ -155,57 +155,64 @@ def test_upload_dataset():
         close_driver(driver)
 
 
-def test_testcarritos(self):
-    self.driver.get("http://localhost:5000/")
-    self.driver.set_window_size(1854, 1048)
-    self.driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
-    self.driver.find_element(
-        By.CSS_SELECTOR,
-        ".list-group-item:nth-child(2) .add-to-cart",
-    ).click()
-    self.driver.find_element(By.ID, "carritoDropdown").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".fa-solid").click()
-    self.driver.find_element(
-        By.CSS_SELECTOR,
-        ".list-group-item:nth-child(2) .add-to-cart",
-    ).click()
-    self.driver.find_element(By.ID, "carritoDropdown").click()
-    self.driver.find_element(
-        By.CSS_SELECTOR,
-        ".list-group-item:nth-child(2) .add-to-cart",
-    ).click()
-    assert (
-        self.driver.switch_to.alert.text
-        == "Este archivo ya está en el carrito."
-    )
-    self.driver.find_element(By.ID, "carritoDropdown").click()
-    self.driver.find_element(
-        By.CSS_SELECTOR,
-        ".flex-grow-1:nth-child(2)",
-    ).click()
-    self.driver.find_element(By.CSS_SELECTOR, ".content").click()
-    self.driver.find_element(
-        By.CSS_SELECTOR,
-        ".list-group-item:nth-child(2) .add-to-cart",
-    ).click()
-    assert (
-        self.driver.switch_to.alert.text
-        == "Este archivo ya está en el carrito."
-    )
-    self.driver.find_element(
-        By.CSS_SELECTOR,
-        ".list-group-item:nth-child(3) .add-to-cart",
-    ).click()
-    self.driver.find_element(
-        By.CSS_SELECTOR,
-        ".list-group-item:nth-child(4) .add-to-cart",
-    ).click()
-    self.driver.find_element(By.ID, "carritoDropdown").click()
-    self.driver.find_element(
-        By.CSS_SELECTOR,
-        ".flex-grow-1:nth-child(1)",
-    ).click()
+def test_testcarritos():
+    driver = initialize_driver()
 
+    try:
+        host = get_host_for_selenium_testing()
+        driver.get(host)
+        driver.set_window_size(1854, 1048)
+        driver.find_element(By.LINK_TEXT, "Sample dataset 4").click()
+        driver.find_element(
+            By.CSS_SELECTOR,
+            ".list-group-item:nth-child(2) .add-to-cart",
+        ).click()
+        driver.find_element(By.ID, "carritoDropdown").click()
+        driver.find_element(By.CSS_SELECTOR, ".fa-solid").click()
+        driver.find_element(
+            By.CSS_SELECTOR,
+            ".list-group-item:nth-child(2) .add-to-cart",
+        ).click()
+        driver.find_element(By.ID, "carritoDropdown").click()
+        driver.find_element(
+            By.CSS_SELECTOR,
+            ".list-group-item:nth-child(2) .add-to-cart",
+        ).click()
+        assert (
+            driver.switch_to.alert.text
+            == "Este archivo ya está en el carrito."
+        )
+        driver.find_element(By.ID, "carritoDropdown").click()
+        driver.find_element(
+            By.CSS_SELECTOR,
+            ".flex-grow-1:nth-child(2)",
+        ).click()
+        driver.find_element(By.CSS_SELECTOR, ".content").click()
+        driver.find_element(
+            By.CSS_SELECTOR,
+            ".list-group-item:nth-child(2) .add-to-cart",
+        ).click()
+        assert (
+            driver.switch_to.alert.text
+            == "Este archivo ya está en el carrito."
+        )
+        driver.find_element(
+            By.CSS_SELECTOR,
+            ".list-group-item:nth-child(3) .add-to-cart",
+        ).click()
+        driver.find_element(
+            By.CSS_SELECTOR,
+            ".list-group-item:nth-child(4) .add-to-cart",
+        ).click()
+        driver.find_element(By.ID, "carritoDropdown").click()
+        driver.find_element(
+            By.CSS_SELECTOR,
+            ".flex-grow-1:nth-child(1)",
+        ).click()
+    finally:
+        # Close the browser
+        close_driver(driver)
 
 # Call the test function
 test_upload_dataset()
+test_testcarritos()
